2f0421133a531e813b12737b382d49171a725e3b
Christopher Owen <christopher.nathaniel.owen@gmail.com>
2024-12-05 16:19:06 -0600

    Add functions for sublineage tiers 1 & 2

--- R/first_appearances.R
+++ R/first_appearances.R
@@ -59,7 +59,7 @@ leading_wwtps_no_doubletons <- first_appearances_by_lin %>%
   filter(!(aliases_removed %in% doubletons$aliases_removed)) %>%
   group_by(wwtp_name) %>%
   summarize(num_first = n()) %>%
-  left_join(idph_sf, by = "wwtp_name") %>%
+  _join(idph_sf, by = "wwtp_name") %>%
   st_as_sf()
 
 leading_wwtps_no_tripletons <- first_appearances_by_lin %>%
@@ -101,34 +101,78 @@ pop_first_model <- lm(log(num_first) ~ log(population_served), data = leading_ww
 summary(pop_first_model)
 plot(pop_first_model)
 
+################################################################################
 ###Looking at Named Variants (VOCs, VOIs, & VUMs)
-
 first_appearances_by_voc <- idph_ww_lin_data %>%
+  select(named_variant_id, weeks_since_start, wwtp_name, nice_wwtp_name, capacity_mgd) %>%
+  distinct() %>%
   group_by(named_variant_id) %>%
   filter(weeks_since_start == min(weeks_since_start)) %>%
-  right_join(idph_sf, by = "wwtp_name") %>%
+  left_join(idph_sf, by = "wwtp_name") %>%
   st_as_sf()
 
 leading_wwtps_voc <- first_appearances_by_voc %>%
   group_by(wwtp_name) %>%
   summarize(num_first = n()) %>%
-  left_join(as.data.frame(idph_sf, by = "wwtp_name")) %>%
+  right_join(as.data.frame(idph_sf, by = "wwtp_name")) %>%
   st_as_sf()
 
 tm_shape(IL) + tm_polygons(alpha = 0.2) +
-  tm_shape(leading_wwtps_voc) + tm_dots(col = "population_served", palette = "viridis", size = "num_first") +
-  tm_layout(main.title = "Grouped to VOCs")
+  tm_shape(leading_wwtps_voc) + tm_dots(col = "log10_pop", breaks = c(3.0, 4.0, 5.0, 6.0, 7.0), palette = "viridis", size = "num_first") +
+  tm_layout(main.title = "Grouped to \nNamed Variants")
 
+################################################################################
+#Repeat analyses with different tax levels:
+
+
+#Top tier#########################################
+first_appearances_by_tier1 <- idph_ww_lin_data %>%
+  select(top_lin_id, weeks_since_start, wwtp_name, nice_wwtp_name, capacity_mgd) %>%
+  distinct() %>%
+  group_by(top_lin_id) %>%
+  filter(weeks_since_start == min(weeks_since_start)) %>%
+  left_join(idph_sf, by = "wwtp_name") %>%
+  st_as_sf()
+
+leading_wwtps_tier1 <- first_appearances_by_tier1 %>%
+  group_by(wwtp_name) %>%
+  summarize(num_first = n()) %>%
+  right_join(as.data.frame(idph_sf, by = "wwtp_name")) %>%
+  st_as_sf()
 
 tm_shape(IL) + tm_polygons(alpha = 0.2) +
-  tm_shape(first_appearances_by_voc) + tm_dots(col = "population_served", palette = "viridis", size = 1) +
-  tm_facets(along = "named_variant_id", free.coords = FALSE) +
-  tm_layout(main.title = "Locations of first appearance") +
-  tm_text(text = "sample_collect_date")
+  tm_shape(leading_wwtps_tier1) + tm_dots(col = "log10_pop", breaks = c(3.0, 4.0, 5.0, 6.0, 7.0), palette = "viridis", size = "num_first") +
+  tm_layout(main.title = "Grouped to Tier 1")
+
+
+#Second tier############################################
+
+idph_ww_lin_data <- idph_ww_lin_data %>%
+  unite("top_lin_id", "sub1", col = "tier2id", sep = ".", remove = FALSE, na.rm = TRUE)
+
+first_appearances_by_tier2 <- idph_ww_lin_data %>%
+  select(tier2id, weeks_since_start, wwtp_name, nice_wwtp_name, capacity_mgd) %>%
+  distinct() %>%
+  group_by(tier2id) %>%
+  filter(weeks_since_start == min(weeks_since_start)) %>%
+  left_join(idph_sf, by = "wwtp_name") %>%
+  st_as_sf()
+
+leading_wwtps_tier2 <- first_appearances_by_tier2 %>%
+  group_by(wwtp_name) %>%
+  summarize(num_first = n()) %>%
+  right_join(as.data.frame(idph_sf, by = "wwtp_name")) %>%
+  st_as_sf()
+
+tm_shape(IL) + tm_polygons(alpha = 0.2) +
+  tm_shape(leading_wwtps_tier2) + tm_dots(col = "log10_pop", breaks = c(3.0, 4.0, 5.0, 6.0, 7.0), palette = "viridis", size = "num_first") +
+  tm_layout(main.title = "Grouped to Tier 2")
 
 
 ################################################################################
+#Examine WRP-wise sampling intensity:
 first_loc_date <- ww_lin_data %>%
   group_by(wwtp_name) %>%
   summarize(first_date = min(sample_collect_date))
+
 
