5a877219f676817bec83c16e067f56141ef17324
Christopher Owen <christopher.nathaniel.owen@gmail.com>
2025-01-02 15:12:04 -0600

    Add comments to geo data imports for clarity

--- R/import_idph_geo.R
+++ R/import_idph_geo.R
@@ -4,17 +4,24 @@
 library(tidyverse)
 library(sf)
 
-#Background Geos (this requires the accompanying files, not just the .shp)
+################################################################################
+#Background Geos (this requires the accompanying files, not just the .shp, to
+#be present in the same directory as the .shp file).
+
+#Read in all IL census tracts (from census.gov):
 il_tracts <- read_sf("../input/cb_2022_17_tract_500k.shp")
+
+#Join census tracts to create state outline:
 IL <- st_union(il_tracts)
 
-#Import wwtp geodata
+################################################################################
+#WWTP geographic data (also includes site metadata):
 idph_sites <- read_csv("../input/IDPH_sites.csv", 
                        col_names = TRUE,
                        col_types = "cccfffff-fd-ddf--id----f---------") %>%
   filter(!is.na(gps_coordinates_lat) & !is.na(gps_coordinates_long))
 
-#Convert to sf and set CRS
+#Convert to sf and set CRS:
 idph_sf <- st_as_sf(idph_sites, 
                     coords = c("gps_coordinates_long", 
                                "gps_coordinates_lat"), 
